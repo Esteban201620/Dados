2f3d682768b69f0633656e3c0a13c5bf5bcf8d9f
Estudiantes <Estudiantes@Labing50617.udistrital.edu.co>
2017-03-15 15:31:59 -0500

    Ahora si es el final jaja

---
+++ Dados/src/dados/Jugador.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dados;
+
+/**
+ *
+ * @author Carlos López
+ */
+public class Jugador {
+    private int billetera;
+    private int apuesta;
+    
+    /* Metodo constructor*/
+    public Jugador() {
+        this.billetera = 1000000;
+    }
+    
+    /* Metodos Getter y Setter*/
+    public int getBilletera(){
+        return billetera;
+    }
+    
+    public void setBilletera(int billetera){
+        this.billetera = billetera;
+    }
+    
+    public int getApuesta(){
+        return apuesta;
+    }
+    
+    public void setApuesta(int apuesta){
+        this.apuesta = apuesta;
+        this.billetera -= apuesta;
+    }
+}
